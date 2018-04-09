fdeff75934b4b22b643ec565ed8a98379f0db9b5
chashikajw <chashikajw007@gmail.com>
2018-04-09 21:06:12 +0530

    add db conncetion file

---
+++ connection.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+var db;
+
+var settings = {
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'stock_diwulapitiya_ps'
+    };
+
+function connectDatabase() {
+    if(!db){
+        db = mysql.createConnection(settings);
+        db.connect(function (err){
+
+            if(err){
+                console.log("error database conncetion");
+            }else{
+                console.log("successfull connected database");
+            }
+        })
+    }
+
+    return db;
+}
+
+module.exports = connectDatabase();
