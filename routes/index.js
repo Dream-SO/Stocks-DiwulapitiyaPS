d1f60b1fe7cef53fb4270b8e51ba8a0b2ccd1bb4
chashikajw <chashikajw007@gmail.com>
2018-04-09 21:07:23 +0530

    add getAllsection list method

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../connection');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+    connection.query('SELECT * FROM section', function (err, rows) {
+        if (err){
+            throw err;
+        }else {
+          console.log(rows);
+          res.render('index', {sections:rows});
+        }
+    });
+
 });
+
 
 module.exports = router;
