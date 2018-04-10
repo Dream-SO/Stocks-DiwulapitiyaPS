49200ebffc80770ceb8ff142c45237f2aea38dcb
chashikajw <chashikajw007@gmail.com>
2018-04-10 21:01:39 +0530

    add table js plugins

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var sectionsRouter = require('./routes/sections');
 var subSectionsRouter = require('./routes/subSections');
+var getItemsStats = require('./routes/getItemsStats');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sections',sectionsRouter);
 app.use('/subSections',subSectionsRouter);
+app.use('/getItemsStats',getItemsStats);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
